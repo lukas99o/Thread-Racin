b2c7cd1ada6ccece1843919bf51484ac8d96ea7f
lukas99o <lukas99o@hotmail.com>
2023-12-11 12:53:20 +0100

    Add project files.

---
+++ Threading/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    internal class Car
+    {
+        public int TopSpeed { get; set; }
+        public double Position {  get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Car(int topSpeed, string brand, string model)
+        {
+            TopSpeed = topSpeed;
+            Brand = brand;
+            Model = model;
+            Position = 0;
+            int startSpeed = 0;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}\t Model: {Model}");
+        }
+
+        public void Race(Car car1, Car car2, Car car3)
+        {
+            double raceDistance = 10.0; // 10 km
+            while (Position < raceDistance)
+            {
+                Console.WriteLine($"{Brand} {Model} is at {Position:F2} km");
+            }
+
+            Thread.Sleep((int)1000.0 / TopSpeed * 3600);
+            Console.WriteLine("The race starts!");
+
+            
+            int carStartingPosition = 0;
+            int carEndingPosition = 10;
+
+            Position += 1;
+
+            if (carStartingPosition == carEndingPosition)
+            {
+                Console.WriteLine($"Model: {Model}\t Brand: {Brand}\n Finished the race! ");
+            }
+            
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+
+
+
+            await Task.Delay(3000);
+            tokenSource.Cancel();
+            string result = await task1;
+            await Console.Out.WriteLineAsync(result);
+
+
+
+            Car car1 = new Car(100, "Volkswagen 1976", "Golf");
+            Car car2 = new Car(250, "Tesla", "Model 3");
+            Car car3 = new Car(420, "Tesla", "Roadster 2");
+
+            Console.WriteLine("These cars will race eachother!");
+            car1.PrintInfo();
+            car2.PrintInfo();
+            car3.PrintInfo();
+
+            Car.Race(car1, car2, car3);
+        }
+    }
+}
